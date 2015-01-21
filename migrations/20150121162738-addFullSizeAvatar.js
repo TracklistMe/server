a35a2cb2660b0cbd9a3007008174f8cb6d272e31
Marco Ziccardi <marco.ziccard@gmail.com>
2015-01-21 21:11:11 +0100

    Added migrations support

---
+++ migrations/20150121162738-addFullSizeAvatar.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+    migration.addColumn(
+      'Users',
+      'fullSizeAvatar',
+      DataTypes.STRING
+    )
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    migration.removeColumn('Users', 'fullSizeAvatar')
+    done();
+  }
+};
