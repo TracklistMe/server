2a386236a9242177d9907c6cc65744cd88b09426
Marco Ziccardi <marco.ziccard@gmail.com>
2015-03-04 01:33:16 +0100

    Refactoring of RabbitMQ APIs to reuse the same connection when possible

--- rabbitmq/rabbitmq.js
+++ rabbitmq/rabbitmq.js
@@ -4,14 +4,42 @@ var config = rootRequire('config/config');
 var amqp = require('amqp');
 
 
+var connectionReady = false;
 var connection = amqp.createConnection( 
-  { host: config.RABBITMQ_HOST,
-    port: config.RABBITMQ_PORT,
-    login: config.RABBITMQ_USERNAME,
-    password: config.RABBITMQ_PASSWORD,
-  });
+{ host: config.RABBITMQ_HOST,
+  port: config.RABBITMQ_PORT,
+  login: config.RABBITMQ_USERNAME,
+  password: config.RABBITMQ_PASSWORD,
+});
+
+connection.on('ready', function() {
+  connectionReady = true;
+  console.log("Connection is ready");
+  if (resultQueueListener) {
+    connection.queue(
+      config.RABBITMQ_RESULT_QUEUE, 
+      {autoDelete: false, durable: true}, 
+      function(queue){
+        console.log("Before subscribe")
+        queue.subscribe(resultQueueListener);
+    }, function(err){
+      console.log(err);
+      console.log("Callback not registered");
+    });
+  }
+});
+
+connection.on('error', function(err) {
+  console.log(err)
+});
+
+var resultQueueListener;
+
+
 
 function sendReleaseToProcess(release) {
+
+    while (!connectionReady) {}
 
     connection.queue(
       config.RABBITMQ_RELEASE_QUEUE, 
@@ -19,25 +47,37 @@ function sendReleaseToProcess(release) {
       function(queue){
         connection.publish(
           config.RABBITMQ_RELEASE_QUEUE, 
-          release.toString(), 
+          JSON.stringify(release, null, 4), 
           {deliveryMode: 2},
           function(err) {
             if (err) console.log("Send failed")
-            else connection.close()
           });
       });
 }
 
-function onReleaseResult(callback) {
-    connection.queue(
-      config.RABBITMQ_RELEASE_QUEUE, 
-      {autoDelete: false, durable: true}, 
-      function(queue){
+exports.sendReleaseToProcess = sendReleaseToProcess;
 
-        queue.subscribe(callback);
-    });
+function onReleaseResult(callback) {
+
+    console.log("REGISTERING CALLBACK");
+    resultQueueListener = callback;
+
+    if (connectionReady) {
+      console.log("Connection is ready");
+      connection.queue(
+        config.RABBITMQ_RESULT_QUEUE, 
+        {autoDelete: false, durable: true}, 
+        function(queue){
+          console.log("before subscribe")
+          queue.subscribe(resultQueueListener);
+      }, function(err){
+        console.log(err);
+        console.log("Callback not registered");
+      });
+    }
+  
 }
 
-exports.sendReleaseToProcess = sendReleaseToProcess;
+exports.onReleaseResult = onReleaseResult;
 
 
