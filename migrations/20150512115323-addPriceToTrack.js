679a006e62adf9167b5f1e5ebdcb5e23c291965f
Marco Ziccardi <marco.ziccard@gmail.com>
2015-05-12 12:02:45 +0200

    Migrations for adding Price to Tracks and Releases

---
+++ migrations/20150512115323-addPriceToTrack.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+    migration.addColumn(
+      'Tracks',
+      'Price', {
+        type: DataTypes.Sequelize.DECIMAL(10, 2),
+        references: "MasterPrices",
+        referenceKey: "price",
+        allowNull: false
+      });
+    done();
+  },
+
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    migration.removeColumn('Tracks', 'Price');
+    done();
+  }
+};
