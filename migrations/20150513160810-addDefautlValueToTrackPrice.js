a27ed210b07467f3d52ca7d81241f9509703eaa1
mziccard <marco.ziccard@gmail.com>
2015-05-13 17:04:08 +0000

    Fixed price migrations for Tracks and Release tables

--- migrations/20150513160810-addDefautlValueToTrackPrice.js
+++ migrations/20150513160810-addDefautlValueToTrackPrice.js
@@ -3,28 +3,23 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
     // add altering commands here, calling 'done' when finished
-    migration.changeColumn(
-      'Tracks',
-      'Price', {
-        type: DataTypes.DECIMAL(10, 2),
-        references: "MasterPrices",
-        referencesKey: "price",
-        allowNull: false,
-        defaultValue: 1.0
-      });
+    //migration.sequelize.query("ALTER TABLE `Tracks` DROP COLUMN `Price`").done(function(err, foo) {
+    migration.sequelize.query("ALTER TABLE `Tracks` UPDATE COLUMN `Price` DECIMAL(10,2) NOT NULL DEFAULT 1.0").done(function(err, foo) {
+    migration.sequelize.query("ALTER TABLE `Tracks` ADD CONSTRAINT `Tracks_ibfk_1` FOREIGN KEY (`Price`) REFERENCES `MasterPrices`(`price`)")
+          .done(function(err, foo) {
+            if (err) throw err;
+            done();
+          });
+        //});
+    }); 
     done();
   },
 
   down: function(migration, DataTypes, done) {
     // add reverting commands here, calling 'done' when finished
-    migration.changeColumn(
-      'Tracks',
-      'Price', {
-        type: DataTypes.DECIMAL(10, 2),
-        references: "MasterPrices",
-        referencesKey: "price",
-        allowNull: false
-      });
-    done();
+    migration.sequelize.query("ALTER TABLE `Tracks` DROP FOREIGN KEY `Tracks_ibfk_1`").done(function(err, foo) {
+      done();
+    });
   }
 };
+
--- migrations/20150513161020-addDefaultValueToReleasePrice.js
+++ migrations/20150513161020-addDefaultValueToReleasePrice.js
@@ -3,28 +3,22 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
     // add altering commands here, calling 'done' when finished
-    migration.changeColumn(
-      'Releases',
-      'Price', {
-        type: DataTypes.DECIMAL(10, 2),
-        references: "MasterPrices",
-        referencesKey: "price",
-        allowNull: false,
-        defaultValue: 1.0
-      });
+    //migration.sequelize.query("ALTER TABLE `Releases` DROP COLUMN `Price`").done(function(err, foo) {
+    migration.sequelize.query("ALTER TABLE `Releases` UPDATE COLUMN `Price` DECIMAL(10,2) NOT NULL DEFAULT 1.0").done(function(err, foo) {
+    migration.sequelize.query("ALTER TABLE `Releases` ADD CONSTRAINT `Releases_ibfk_1` FOREIGN KEY (`Price`) REFERENCES `MasterPrices`(`price`)")
+          .done(function(err, foo) {
+		console.log("DONE");
+            done();
+          });
+      //});
+    }); 
     done();
   },
 
   down: function(migration, DataTypes, done) {
     // add reverting commands here, calling 'done' when finished
-    migration.changeColumn(
-      'Releases',
-      'Price', {
-        type: DataTypes.DECIMAL(10, 2),
-        references: "MasterPrices",
-        referencesKey: "price",
-        allowNull: false
-      });
-    done();
+    migration.sequelize.query("ALTER TABLE `Releases` DROP FOREIGN KEY `Releases_ibfk_1`").done(function(err, foo) {
+      done();
+   });
   }
 };
