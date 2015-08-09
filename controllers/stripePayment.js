b29464ff1b7915341e98921d671be92ff507b501
Marco Ziccardi <marco.ziccard@gmail.com>
2015-08-09 16:01:12 +0200

    Linted controllers/stripePayment.js

--- controllers/stripePayment.js
+++ controllers/stripePayment.js
@@ -1,18 +1,16 @@
 'use strict';
 var Q = require('q');
-var fs = require('fs-extra');
 var config = rootRequire('config/config');
-var fileUtils = rootRequire('utils/file-utils');
 var authenticationUtils = rootRequire('utils/authentication-utils');
 var model = rootRequire('models/model');
-var cloudstorage = rootRequire('libs/cdn/cloudstorage');
 var stripe = require('stripe')(config.STRIPE_PRIVATE);
-
 
 module.exports.controller = function(app) {
 
-  // FIND THE PRICE OF A TRACK given trackID, and currencyID
-
+  /**
+   * Find the price of a track given idTrack and idCurrency
+   * TODO move to Track model
+   */
   function findPriceTrack(idTrack, idCurrency) {
     var deferred = Q.defer();
     model.Track.find({
@@ -26,15 +24,18 @@ module.exports.controller = function(app) {
           MasterPrice: track.Price
         }
       }).then(function(convertedPrice) {
-        console.log("FINAL CONVERTED PRICE FOR TRACK " + convertedPrice.price)
-        deferred.resolve(convertedPrice.price)
-      })
-    })
+        console.log('FINAL CONVERTED PRICE FOR TRACK ' + convertedPrice.price);
+        deferred.resolve(convertedPrice.price);
+      });
+    });
     return deferred.promise;
   }
 
-  // Find the price of a release given Id and currency ID
-  // if the release doesn't have a price, lunches the search on all the subtracks
+  /**
+   * Find the price of a release given idRelease and idCurrency
+   * If the release doesn't have a price, return the sum of tracks prices
+   * TODO: move to model
+   */
   function findPriceRelease(idRelease, idCurrency) {
 
     var deferred = Q.defer();
@@ -48,157 +49,129 @@ module.exports.controller = function(app) {
         attributes: ['id', 'Price']
       }
     }).then(function(release) {
-      if (release.Price) { // I have a price and is not null (check the behavior when PRICE in db is 0.00)
+      if (release.Price) {
+        // A price for the release exists 
+        // (check the behavior when PRICE in db is 0.00)
         model.ConvertedPrice.find({
           where: {
-
             CurrencyId: idCurrency,
             MasterPrice: release.Price
           }
         }).then(function(convertedPrice) {
-          console.log("FINAL CONVERTED PRICE FOR RELEASE " + convertedPrice.price)
-          deferred.resolve(convertedPrice.price)
-        })
+          console.log(
+            'FINAL CONVERTED PRICE FOR RELEASE ' +
+            convertedPrice.price);
+          deferred.resolve(convertedPrice.price);
+        });
       } else {
-        console.log("I DON'T HAVE A PRICE FOR THIS RELEASE")
+        // A price for the release does not exist
         var promisesArray = [];
         for (var t = 0; t < release.Tracks.length; t++) {
-          promisesArray.push(findPriceTrack(release.Tracks[t].id, idCurrency));
-
+          promisesArray.push(
+            findPriceTrack(release.Tracks[t].id, idCurrency));
         }
-        // SUM ALL THE TRACKS THAT BELONGS TO THE ARRAY
+        // Sum the prices of all the tracks in the release
         Q.all(promisesArray).then(function(results) {
-            var sumPrices = 0;
-            for (var r = 0; r < results.length; r++) {
-              sumPrices += results[r];
-            }
-            deferred.resolve(sumPrices);
-          }, console.error)
-          // THE RELEASE DOESN't HAVE A PRICE SO WE NEED TO CALCULATE THE SUB PRICES 
-          /* 
-          var groupPromise = Q.all([doThis(), doThat()])
-          groupPromise.then(function(results) {}, console.error)
-          */
+          var sumPrices = 0;
+          for (var r = 0; r < results.length; r++) {
+            sumPrices += results[r];
+          }
+          deferred.resolve(sumPrices);
+        }, console.error);
+        /* 
+        var groupPromise = Q.all([doThis(), doThat()])
+        groupPromise.then(function(results) {}, console.error)
+        */
       }
-    })
+    });
     return deferred.promise;
   }
 
   /**
-   * Ensures current user is the owner of the company
-   * Company ID must be passed as req.body.companyId
-    
-  app.get('/labels/search/:searchString', function(req, res, next) {
-      var searchString = req.params.searchString;
-      model.Label.findAll({
-          where: ["displayName LIKE ?", "%" + searchString + "%"]
-      }).then(function(labels) {
-          res.send(labels);
-      });
-  });
-
-  /**
    * POST /payment/:idToken
-   * Return the list of labels whose displayName exactly matches the search string
+   * Return the list of labels whose displayName exactly matches the search
+   * string
    */
-  app.post('/payments', authenticationUtils.ensureAuthenticated, function(req, res, next) {
-    var token = req.body.token;
-    var userId = req.user;
-    var currency = req.body.currency;
-    var cart = req.body.cart;
+  app.post('/payments',
+    authenticationUtils.ensureAuthenticated,
+    function(req, res, next) {
+      var token = req.body.token;
+      var userId = req.user;
+      var currency = req.body.currency;
+      var cart = req.body.cart;
 
-
-    // RECALCULATE THE PRICE - serverside.
-    var promisesArray = [];
-    for (var i = 0; i < cart.length; i++) {
-      if (cart[i].id.indexOf('track') > -1) { // THIS IS A TRACK 
-        promisesArray.push(findPriceTrack(cart[i].id.split("-").pop(), currency.id)) //get the ID from release
-      }
-      if (cart[i].id.indexOf('release') > -1) { // THIS IS A RELEASE 
-        promisesArray.push(findPriceRelease(cart[i].id.split("-").pop(), currency.id)); //get the ID from release
-      }
-    }
-
-    Q.all(promisesArray).then(function(results) {
-      console.log("ALL SETTLEs")
-      var sumPrices = 0.0;
-      for (var r = 0; r < results.length; r++) {
-
-        sumPrices += (results[r] * cart[r].quantity) // multiply by quantity ;
-      }
-
-      sumPrices = sumPrices.toFixed(2); // SET TO DECIMALS
-      sumPrices = sumPrices * 100;
-
-      // TODO REMEMBER TO ADD THE VAT 
-
-      console.log("FINAL PRICE " + sumPrices)
-
-      //I HAVE THE TOTAL PRICE 
-      //
-      model.User.find({
-        where: {
-          id: userId
+      // Recompute the price for the currency.
+      var promisesArray = [];
+      for (var i = 0; i < cart.length; i++) {
+        if (cart[i].id.indexOf('track') > -1) {
+          // Price of a track 
+          // Get track ID
+          promisesArray.push(
+            findPriceTrack(cart[i].id.split('-').pop(), currency.id));
         }
-      }).then(function(user) {
-        if (!user) {
-          var err = new Error();
-          err.status = 401;
-          err.message = "You can't buy because you are not an user! ";
-          return next(err);
+        if (cart[i].id.indexOf('release') > -1) {
+          // Price of a release
+          // Get release ID
+          promisesArray.push(
+            findPriceRelease(cart[i].id.split('-').pop(), currency.id));
+        }
+      }
+
+      Q.all(promisesArray).then(function(results) {
+        var sumPrices = 0.0;
+        for (var r = 0; r < results.length; r++) {
+          // multiply by quantity
+          sumPrices += (results[r] * cart[r].quantity);
         }
 
-        stripe.customers.create({
-          source: token,
-          email: user.email
-        }).then(function(customer) {
-          console.log("CUSTOMER RECEIVED ");
-          // console.log(customer);
+        sumPrices = sumPrices.toFixed(2);
+        sumPrices = sumPrices * 100;
 
-          var cloudURL = ''
-          var packURL = ''
+        // TODO Add the VAT
+        console.log('FINAL PRICE ' + sumPrices);
 
+        model.User.find({
+          where: {
+            id: userId
+          }
+        }).then(function(user) {
+          if (!user) {
+            var err = new Error();
+            err.status = 401;
+            err.message = 'You can\'t buy because you are not an user!';
+            return next(err);
+          }
 
-          stripe.charges.create({
-            amount: sumPrices,
-            currency: currency.shortname, //ISO NAME
-            customer: customer.id,
-            metadata: {
-              // CHANCE TO PIGGy BACK HERE SOME ADDITIONAL INFORMATION
-              // TODO consider if we want to have a TRANSACTION ID in our db
-            },
-          }, function(err, charge) {
+          stripe.customers.create({
+            source: token,
+            email: user.email
+          }).then(function(customer) {
+            console.log('CUSTOMER RECEIVED ');
 
-            if (!err) {
-              // GOT IT ! 
-              // PAYMENT as been done correctly, should move the just bought tracks to the library of the user
-              // RELEASE should be expanded into tracks.
-              // 
-              console.log("PAYMENT RECEIVED! ")
-              res.send(charge);
-            }
+            stripe.charges.create({
+              amount: sumPrices,
+              currency: currency.shortname,
+              customer: customer.id,
+              metadata: {
+                // CHANCE TO PIGGY BACK HERE SOME ADDITIONAL INFORMATION
+                // TODO consider if we want to have a TRANSACTION ID in our db
+              },
+            }, function(err, charge) {
+
+              if (!err) {
+                // GOT IT ! 
+                // PAYMENT as been done correctly, should move the just b
+                // ought tracks to the library of the user
+                // RELEASE should be expanded into tracks.
+                // 
+                console.log('PAYMENT RECEIVED! ');
+                res.send(charge);
+              }
+            });
+            // TODO Here fetch the cloud link to the 
+            // product that has been purcheased.
           });
-
-
-          // Here fetch the cloud link to the product that has been purcheased.
-
         });
-
-
-      });
-
-
-
-
-    }, console.error)
-
-
-
-
-
-
-  });
-
-
-
-} /* End of payment controller */
+      }, console.error);
+    });
+}; /* End of payment controller */
