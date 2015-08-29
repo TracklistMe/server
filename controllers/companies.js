bb6105e4944c0d8044968c48775a03178c8e3542
Nicola Bortignon <info@nicolabortignon.com>
2015-08-28 18:54:56 -0700

    Minor bug fixes in companies/royalties and implemented labels/royalties

--- controllers/companies.js
+++ controllers/companies.js
@@ -265,7 +265,7 @@ module.exports.controller = function(app) {
     function(req, res) {
       console.log(req.params.startDate);
       var startDate = moment(req.params.startDate, "DD-MM-YYYY", true);
-      var endDate = moment(req.params.endDate, "DD-MM-YYYY", true);
+      var endDate = moment(req.params.endDate, "DD-MM-YYYY", true).endOf('day');
 
       console.log("IS VALID: " + startDate.isValid());
       if (startDate && startDate.isValid()) {
@@ -301,8 +301,8 @@ module.exports.controller = function(app) {
             $between: [startDate, endDate],
           }
         },
-        order: 'Transaction.createdAt',
-        group: ['Transaction.LabelId', 'Transaction.createdAt']
+        order: 'dataColumn',
+        group: ['Transaction.LabelId', 'dataColumn']
       }).then(function(results) {
         res.send(results);
       });
@@ -336,18 +336,7 @@ module.exports.controller = function(app) {
     imagesController.getImageFactory('logo', helper));
 
 
-  function isValidDate(date) {
-    var matches = /^(\d{2})[-](\d{2})[-](\d{4})$/.exec(date);
-    if (matches == null) return false;
-    var d = matches[2];
-    var m = matches[1] - 1;
-    var y = matches[3];
 
-    var composedDate = new Date(y, m, d);
-    return composedDate.getDate() == d &&
-      composedDate.getMonth() == m &&
-      composedDate.getFullYear() == y;
-  }
 
 
 }; /* End of companies controller */
